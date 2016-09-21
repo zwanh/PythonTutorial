1b2b517ebb8395a15ee4bb142fb04254055c37e5
wheels1991 <ugvlpl@qq.com>
2016-09-21 23:07:14 +0800

    add tcp trajectory generate func

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 import numpy as np
 from trajectoryInterpolation import *		#import all class and functions
 from dogPlatform import *
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as animation
+
+def genDogLegTcps(dog):
+	'''generate a series of dog legTcps'''
+	
+	tcp = dog.getLegTcp()
+	dTcp1 = np.array([[0, 30, 50, 0],\
+			  [0, 0, 0, 0],\
+			  [0, 0, 0, 0],\
+			  [0, 0, 0, 0]])
+	dTcp2 = np.array([[0, 50, 0, 0],\
+			  [0, 0, 0, 0],\
+			  [0, 0, 0, 0],\
+			  [0, 0, 0, 0]])
+	A = tcpTrajectoryInterp(tcp, dTcp1, dTcp2)
+	Tcps = [tcpOnTrajectory(A, t/2.) for t in range(3)]
+	return Tcps
 
 dog = dogPlatform()
-
-tcp = np.array([[-150, 200, -130, 1],\
-               [250, 250, -130, 1],\
-               [-250, -250, -130, 1],\
-               [150, -200, -130, 1]])
-
 
 initLegPose = np.array([[0, -45, 90],\
 			[0, -45, 90],\
 			[0, -45, 90],\
 			[0, -45, 90]])
 dog.setLegPose(initLegPose)
-#dogShow(dog)
-tcp = dog.getLegTcp()
-print('tcp', tcp)
-dTcp1 = np.array([[0, 10, 20, 0],\
-		  [0, 10, 20, 0],\
-		  [0, 10, 20, 0],\
-		  [0, 10, 20, 0]])
-
-dTcp2 = np.array([[0, 20, 0, 0],\
-		  [0, 20, 0, 0],\
-		  [0, 20, 0, 0],\
-		  [0, 20, 0, 0]])
-A = tcpTrajectoryInterp(tcp, dTcp1, dTcp2)
-t = 1 
-newTcp = tcpOnTrajectory(A,t)
-print('newTcp', newTcp)
+dogShow(dog)
+print 'init pos', dog.getLegTcp()
+Tcps = genDogLegTcps(dog)
+dog.setLegTcp(Tcps[1])
+dogShow(dog)
+print 'new pos', dog.getLegTcp()
