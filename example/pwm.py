bfbdda51b8bebd016a057efc926e8e0313c00e51
wheels1991 <ugvlpl@qq.com>
2016-09-23 17:17:35 +0800

    repair WiringPi pwm frequency set, but can't set 50Hz, the least frequency is about 500Hz

--- example/pwm.py
+++ example/pwm.py
@@ -1,19 +1,11 @@
 #this is an example of pwm
 
-import wiringpi2 as gpio
-
-PIN_TO_PWM = 1
-OUT_PUT = 1
+import wiringpi as gpio
 
 gpio.wiringPiSetup()
-gpio.pinMode(PIN_TO_PWM, OUT_PUT)
-
-gpio.softPwmCreate(PIN_TO_PWM, 0, 100)
-
-for time in range(0, 4):
-	for brightness in range(0, 100):
-		gpio.softPwmWrite(PIN_TO_PWM, brightness)
-		gpio.delay(10)
-	for brightness in reversed(range(0, 100)):
-		gpio.softPwmWrite(PIN_TO_PWM, brightness)
-		gpio.delay(10)
+gpio.pinMode(1, 2)
+gpio.pwmSetClock(3840)
+gpio.pwmSetRange(200000)
+gpio.pwmWrite(1, 20000)
+#gpio.pinMode(4, 2)
+#gpio.pwmWrite(4, 512)
