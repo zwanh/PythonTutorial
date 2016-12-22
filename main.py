b8d7813a2902e98b8f8171d9844d674046df33c5
wheels1991 <ugvlpl@qq.com>
2016-12-22 21:17:46 +0800

    serial port remote control

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#from __future__ import *
 import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
@@ -9,32 +8,28 @@ import threading
 from gaitGenerator import *
 import time
 import Adafruit_PCA9685
+import serial
 
-frameNum = 5
-stepTime = 25		#units:ms
+frameNum = 15
+stepTime = 100		#units:ms
 enableAni = False	#enable animation or not	
 alfa = 0.5		#smooth coefficiency
-
+dog = dogPlatform()
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
-
-bias = np.array([[-7, 0, -19],\
-				 [-3, -2, 18],\
-				 [-10, -8, -10],\
-				 [-6, 4, 10]])
-
+	
 def set_servo_angle(angle):
 	for i in range(0, 12):
-		pwm.set_pwm(i, i, int(angle[i]*2.26+105))
-
+		pwm.set_pwm(i, i, int(angle[i]*2.276+102.4))
 
 def motorDriver(joints):
 	print(joints)
+	global bias
 	joints = joints + bias
-	leg1 = [90 - joints[0][0], 90 - joints[0][1], 90 - joints[0][2]]
-	leg2 = [90 - joints[1][0], 90 + joints[1][1], 90 + joints[1][2]]
-	leg3 = [90 - joints[2][0], 90 - joints[2][1], 90 - joints[2][2]]
-	leg4 = [90 - joints[3][0], 90 + joints[3][1], 90 +  joints[3][2]]
+	leg1 = [90 + joints[0][0], 90 - joints[0][1], 90 - joints[0][2]]
+	leg2 = [90 + joints[1][0], 90 + joints[1][1], 90 + joints[1][2]]
+	leg3 = [90 + joints[2][0], 90 - joints[2][1], 90 - joints[2][2]]
+	leg4 = [90 + joints[3][0], 90 + joints[3][1], 90 +  joints[3][2]]
 	leg = leg1 + leg2 + leg3 + leg4
 	set_servo_angle(leg)	
 
@@ -60,8 +55,8 @@ def update_fig(num, Tcps, dTcps, dog, lines = None):
 		dog.setLegTcp(tcp)
 		legPose = dog.getLegPose()
 		motorDriver(legPose)
-		t = threading.Timer(stepTime *0.001, update_fig, [(num + 1) % (2 * frameNum), Tcps, dTcps, dog])
-		t.start()
+		timer = threading.Timer(stepTime *0.001, update_fig, [(num + 1) % (2 * frameNum), Tcps, dTcps, dog])
+		timer.start()
 
 def plotData(tcp, dog):
 	dog.setLegTcp(tcp)
@@ -77,35 +72,73 @@ def plotData(tcp, dog):
 	data = [leg[0], leg[1], leg[2], leg[3], fix]
 	return data
 
-dog = dogPlatform()
+global instruction
+instruction = 0;
 
-initLegPose1 = np.array([[0, 0, 0],\
-						[0, 0, 0],\
-						[0, 0, 0],\
-						[0, 0, 0]])
-dog.setLegPose(initLegPose1)
-motorDriver(initLegPose1)
+def readSerial(sp):
+	while True:
+		data = sp.readline()
+		sp.write(data)
+		global instruction
+		if data == "Forward":
+			instruction = 0
+		elif data == "Backward":
+			instruction = 1
+		if data:
+			instruction = 2
+			print instruction, data
+			break
+		time.sleep(0.1)
+	
+def myMain():
+	global bias
+	bias = np.array([[-0, 0, -0],\
+					 [-0, -0, 0],\
+					 [-0, -0, -0],\
+					 [-0, 0, 0]])
+	
+	
+	initLegPose = np.array([[0, 0, 0],\
+	 						 [0, 0, 0],\
+							 [0, 0, 0],\
+							 [0, 0, 0]])
+	motorDriver(initLegPose)
+	
+	#init plot
+	data = plotData(dog.getLegTcp(), dog)
+	
+	pace = gaitPace(-145)
+	trot = gaitTrot(-145)
+	walk = gaitWalk(-145)
+	Tcps = pace.getTcps(15, 40, frameNum)
+	dTcps = pace.getTcpsRelative(15, 40, frameNum)
+	#show figure and animation, need a screen
+	if (enableAni) :
+		fig = plt.figure()
+		ax = p3.Axes3D(fig)
+		ax.set_xlim3d([-200,200])
+		ax.set_ylim3d([-200,200])
+		ax.set_zlim3d([-200,200])
+		ax.set_title('dog simulation')
+		#ax.axis('equal')
+		lines = [ax.plot(dat[0,0:], dat[1,0:], dat[2,0:])[0] for dat in data]
+		dogAni = animation.FuncAnimation(fig, update_fig,frameNum * 2, repeat = True, fargs = (Tcps, dTcps, dog, lines), interval = stepTime)
+		plt.show()
+	#don't show figure and animation, not need a screen
+	else :
+		print("init")
+		update_fig(0, Tcps, dTcps,dog)
 
-#init plot
-data = plotData(dog.getLegTcp(), dog)
-pace = gaitPace(-150)
-trot = gaitTrot(-80)
-walk = gaitWalk(-80)
-Tcps = pace.getTcps(15, 40, frameNum)
-dTcps = pace.getTcpsRelative(15, 40, frameNum)
-#show figure and animation, need a screen
-if (enableAni) :
-	fig = plt.figure()
-	ax = p3.Axes3D(fig)
-	ax.set_xlim3d([-200,200])
-	ax.set_ylim3d([-200,200])
-	ax.set_zlim3d([-200,200])
-	ax.set_title('dog simulation')
-	#ax.axis('equal')
-	lines = [ax.plot(dat[0,0:], dat[1,0:], dat[2,0:])[0] for dat in data]
-	dogAni = animation.FuncAnimation(fig, update_fig,frameNum * 2, repeat = True, fargs = (Tcps, dTcps, dog, lines), interval = stepTime)
-	plt.show()
-#don't show figure and animation, not need a screen
-else :
-	print("init")
-	update_fig(0, Tcps, dTcps,dog)
+if __name__== '__main__':
+	serialPort = serial.Serial('/dev/ttyAMA0', 9600, timeout = 0.5)
+	thread_serial = threading.Thread(target = readSerial, args = (serialPort,))
+	thread_serial.setDaemon(True)
+	thread_serial.start()
+#	thread_serial.join()
+	while True:
+		time.sleep(0.5)
+		if instruction == 2:
+			instruction = 0;
+			myMain();
+			print "Into my main"
+			break
